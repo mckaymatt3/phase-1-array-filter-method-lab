d3560e86668c28779bc74e14319b258f0be92923
mckaymatt3 <mckaymatt@outlook.com>
2022-02-04 10:14:43 -0500

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Code your solution here
+function findMatching(array, string) {
+    let newArray = [...array];
+    let newString = string;
+    let checkArray = newArray.filter((newArray => newArray.toUpperCase() === newString.toUpperCase()));
+        return checkArray
+}
+
+// function findMatching(array,string) {
+
+function fuzzyMatch(array,string) {
+    let newArray = [...array];
+    let newString = string;
+    let checkArray = newArray.filter((newArray => newArray[0] === newString[0]));
+        return checkArray
+}
+ 
+// function matchName(array,string) {
+//     let newArray = [...array];
+//     let newString = string;
+//     let checkArray = newArray.filter((newArray => newArray === newString))
+//         return checkArray
+// }
+
+function matchName (arrayOfObjects, string) {
+    return arrayOfObjects.filter(function(element) {
+        return element.name === string
+    })
+}
