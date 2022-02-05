1b494e0eeb42be222bdb00d073c91a1bc3ef5ce5
mckaymatt3 <mckaymatt@outlook.com>
2022-02-05 18:24:25 -0500

    commit tree bb2365c231ebe7161012844ff3cbfb36a0abb551

--- index.js
+++ index.js
@@ -1,29 +1,48 @@
 // Code your solution here
-function findMatching(array, string) {
-    let newArray = [...array];
-    let newString = string;
-    let checkArray = newArray.filter((newArray => newArray.toUpperCase() === newString.toUpperCase()));
-        return checkArray
-}
 
-// function findMatching(array,string) {
+//function findMatching
 
-function fuzzyMatch(array,string) {
-    let newArray = [...array];
-    let newString = string;
-    let checkArray = newArray.filter((newArray => newArray[0] === newString[0]));
-        return checkArray
-}
- 
-// function matchName(array,string) {
+// function findMatching(array, string) {
 //     let newArray = [...array];
 //     let newString = string;
-//     let checkArray = newArray.filter((newArray => newArray === newString))
+//     let checkArray = newArray.filter((newArray => newArray.toUpperCase() === newString.toUpperCase()));
 //         return checkArray
 // }
 
-function matchName (arrayOfObjects, string) {
-    return arrayOfObjects.filter(function(element) {
-        return element.name === string
+function findMatching(array, string) {
+    return array.filter(function (element) {
+        return element.toUpperCase() === string.toUpperCase()
     })
 }
+
+
+// function fuzzyMatach
+
+// function fuzzyMatch(array,string) {
+//     let newArray = [...array];
+//     let newString = string;
+//     let checkArray = newArray.filter((newArray => newArray[0] === newString[0]));
+//         return checkArray
+// }
+ 
+function fuzzyMatch(array,string) {
+    return array.filter (function (element) {
+        return element[0] === string[0]
+    })
+}
+
+
+// function matchName
+
+function matchName (arrayOfObjects, string) {
+    return arrayOfObjects.filter(function (element) {
+        return element.name === string;
+    })
+}
+
+// function matchName (arrayOfObjects, string) {
+//     let checkArray = arrayOfObjects.filter(function (element) {
+//         return element.name === string;
+//     })
+//     return checkArray
+// }
